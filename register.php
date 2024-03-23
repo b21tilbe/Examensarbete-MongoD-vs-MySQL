f17fead0da6ea5ff498e5173a2e3e62c3d177476
b21tilbe <102578990+b21tilbe@users.noreply.github.com>
2024-03-23 17:05:19 +0100

    Create register.php
    Created a register code that enables new users to sign up to the application

---
+++ register.php
@@ -0,0 +1,18 @@
+<?php
+include("connection.php");
+
+if(isset($_POST['register'])) {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    
+    $sql = "INSERT INTO login (username, password) VALUES ('$username', '$password')";
+    
+    if(mysqli_query($conn, $sql)) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+
+mysqli_close($conn);
+?>
