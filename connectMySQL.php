b58caba9aceb1e8732e74f7c924cbbccf8c137bc
b21tilbe <102578990+b21tilbe@users.noreply.github.com>
2024-03-23 15:16:15 +0100

    Update and rename connectMySQL.php to connection.php
    Fixed a new database and better connection possibility

--- connectMySQL.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $user = 'root';
-    $pass = '';
-    $db = 'mysql database';
-
-    $db = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
-
-    echo"Connected";
-?>
