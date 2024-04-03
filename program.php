1f738a93cbd4963bed2ca755164ee6817bb3b643
b21tilbe <102578990+b21tilbe@users.noreply.github.com>
2024-04-03 14:25:03 +0200

    Update program.php
    New program cards to select program type with

--- program.php
+++ program.php
@@ -19,8 +19,36 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0">
 </head>
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+
+        var cards = document.querySelectorAll(".card");
+
+        cards.forEach(function(card) {
+            card.addEventListener("click", function(event) {
+                var selectedValue = event.currentTarget.dataset.cardNumber;
+
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "save_choice.php", true);
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === XMLHttpRequest.DONE) {
+                        if (xhr.status === 200) {
+                            console.log("Användarens val har sparats på servern.");
+                            alert("Du valde kort " + selectedValue);
+                        } else {
+                            console.error("Ett fel uppstod när användarens val skulle sparas på servern.");
+                        }
+                    }
+                };
+                xhr.send("selected_value=" + selectedValue);
+            });
+        });
+    });
+</script>
+
 <body>
-    <div class="username">Welcome <?php echo $username; ?></div>
+    <h1 id="ProgramH1">Type of workout</h1>
     <header>
         <nav>
             <ul class="nav-bar">
@@ -38,19 +66,36 @@
             </ul>
         </nav>
     </header>
-    <div id="indexPicture">
-        <img src="/pictures/index.jpg" alt="workout">
-        <p id="indexP">PWR</p>
-        <p id="indexPUnder">POWERWORKOUT</p>
-    </div>
-    <div id="indexWeeks">
-        <p>Week x4</p>
-    </div>
-    <div id="indexUndertext">
-        <p>Elevate your fitness journey with programs uniquely designed to maximize your results.</p>
-    </div>
-    <div class="Dots">
-        <p>○●○○</p>
+    <div id="programBody">
+        <div class="cards">
+            <div class="card card1" data-card-number="1">
+                <div class="cardContainer">
+                    <img src="/pictures/strength.jpg" alt="strength">
+                </div>
+                <div class="details">
+                    <h3>Strength</h3>
+                    <p>Lorem Ipsum</p>
+                </div>
+            </div>
+            <div class="card card2" data-card-number="2">
+                <div class="cardContainer">
+                    <img src="/pictures/strength.jpg" alt="strength">
+                </div>
+                <div class="details">
+                    <h3>Kondition</h3>
+                    <p>Lorem Ipsum</p>
+                </div>
+            </div>
+            <div class="card card3" data-card-number="3">
+                <div class="cardContainer">
+                    <img src="/pictures/strength.jpg" alt="strength">
+                </div>
+                <div class="details">
+                    <h3>Uthållighet</h3>
+                    <p>Lorem Ipsum</p>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
