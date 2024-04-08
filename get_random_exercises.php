05648a5cbe04c4ffa5eac56c9202058e93f55659
b21tilbe <102578990+b21tilbe@users.noreply.github.com>
2024-04-08 16:00:18 +0200

    Update get_random_exercises.php
    Added randomizer for reps depending on the selected workout style

--- get_random_exercises.php
+++ get_random_exercises.php
@@ -1,18 +1,43 @@
 <?php
-   include("connection.php");
+include("connection.php");
 
-    $sql = "SELECT * FROM träningsövning ORDER BY RAND() LIMIT 5";
+if (isset($_GET['selected_card'])) {
+    $selectedCard = $_GET['selected_card'];
+
+    $sql = "SELECT * FROM träningsövning ORDER BY RAND() LIMIT 6";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         echo "<table border='1'>";
-        echo "<tr><th>ÖvningsNamn</th><th>Typ</th></tr>";
+        echo "<tr><th>Exercise</th><th>Typ</th><th>Reps</th></tr>";
         while($row = $result->fetch_assoc()) {
-            echo "<tr><td>" . $row["ÖvningsNamn"] . "</td><td>" . $row["Typ"] . "</td></tr>";
+            echo "<tr><td>" . $row["ÖvningsNamn"] . "</td><td>" . $row["Typ"] . "</td><td>";
+
+            switch ($selectedCard) {
+                case 'Powerlifting':
+                    $reps = rand(4, 6);
+                    break;
+                case 'Strength':
+                    $reps = rand(8, 12);
+                    break;
+                case 'Endurance':
+                    $reps = rand(15, 25);
+                    break;
+                default:
+                    $reps = "N/A";
+                    break;
+            }
+
+            echo $reps;
+            echo "</td></tr>";
         }
         echo "</table>";
     } else {
-        echo "Inga resultat hittades";
+        echo "No results found";
     }
-    $conn->close();
+} else {
+    echo "No card found";
+}
+
+$conn->close();
 ?>
