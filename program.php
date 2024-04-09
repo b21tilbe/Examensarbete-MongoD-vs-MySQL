6a3b8dca825ae5af3b0aa06165d27b1b726b7b96
b21tilbe <102578990+b21tilbe@users.noreply.github.com>
2024-04-09 14:48:18 +0200

    Update program.php
    Updated saveSelection function to be able to have a user history

--- program.php
+++ program.php
@@ -18,6 +18,7 @@
     <link rel="stylesheet" href="FinalProject.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
     <h1 id="ProgramH1">Type of workout</h1>
@@ -29,8 +30,7 @@
                 <span class="menu">
                     <li><a href="loggedIn.php">Home</a></li>
                     <li><a href="program.php">Program</a></li>
-                    <li><a href="workout.php">Workout</a></li>
-                    <li><a href="#">Profile</a></li>
+                    <li><a href="profile.php">Profile</a></li>
                     <li><a href="FinalProject.php">Log out</a></li>
                     <label for="check" class="close-menu"><i class="fas fa-times"></i></label>
                 </span>
@@ -40,8 +40,21 @@
     </header>
     <script>
         function saveSelection(selection) {
+            var username = "<?php echo $username; ?>";
             localStorage.setItem('selectedCard', selection);
-            window.location.href = 'workout.php';
+            
+            $.ajax({
+                url: 'save_program_selection.php',
+                method: 'POST',
+                data: { programName: selection, username: username },
+                success: function(response) {
+                    console.log(response);
+                    window.location.href = 'workout.php';
+                },
+                error: function(xhr, status, error) {
+                    console.error(error);
+                }
+            });
         }
     </script>
     <div id="programBody">
