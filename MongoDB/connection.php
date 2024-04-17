bb1f1efecb8a0ba060aaccaf946363d49852fe73
b21tilbe <102578990+b21tilbe@users.noreply.github.com>
2024-04-17 15:36:00 +0200

    Create connection.php
    Created connection to MongoDB server

---
+++ MongoDB/connection.php
@@ -0,0 +1,9 @@
+<?php
+    try {
+        $conn = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+    } catch (MongoDBDriverExceptionException $e) {
+        echo 'Failed connection!<br /><br />';
+        echo $e->getMessage();
+    exit();
+    }
+?>
