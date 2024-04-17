cc96c3a21bf84d277d97473cb3bc1d58c608cc9f
b21tilbe <102578990+b21tilbe@users.noreply.github.com>
2024-04-17 16:26:34 +0200

    Update login.php
    Fixed the problem with $username not showing up in loggedIn.php

--- MongoDB/login.php
+++ MongoDB/login.php
@@ -1,30 +1,33 @@
 <?php
-  include("connection.php");
-  
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      $databaseName = 'database2';
-      $collectionName = 'login';
-      
-      $username = $_POST["user"];
-      $password = $_POST["pass"];
-      
-      $filter = ['username' => $username, 'password' => $password];
-      $query = new MongoDB\Driver\Query($filter);
-      $rows = $conn->executeQuery("$databaseName.$collectionName", $query);
-      
-      $match = false;
-      foreach ($rows as $row) {
-          $match = true;
-          break;
-      }
-      
-      if ($match) {
-          echo '<script>window.location.href = "loggedIn.php";</script>';
-          exit();
-      } else {
-          echo '<script>alert("Invalid login.");</script>';
-          echo '<script>window.location.href = "FinalProject.php";</script>';
-          exit();
-      }
-  }
+    include("connection.php");
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $databaseName = 'database2';
+        $collectionName = 'login';
+        
+        $username = $_POST["user"];
+        $password = $_POST["pass"];
+        
+        $filter = ['username' => $username, 'password' => $password];
+        $query = new MongoDB\Driver\Query($filter);
+        $rows = $conn->executeQuery("$databaseName.$collectionName", $query);
+        
+        $match = false;
+        foreach ($rows as $row) {
+            $match = true;
+            break;
+        }
+        
+        if ($match) {
+            session_start();
+            $_SESSION['username'] = $username;
+            
+            header("Location: loggedIn.php");
+            exit();
+        } else {
+            echo '<script>alert("Invalid login.");</script>';
+            header("Location: FinalProject.php");
+            exit();
+        }
+    }
 ?>
