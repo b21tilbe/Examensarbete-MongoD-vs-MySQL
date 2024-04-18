3ccc64546b86cf04d8d5ea62b4971e7bf55be479
b21tilbe <102578990+b21tilbe@users.noreply.github.com>
2024-04-18 19:18:14 +0200

    MongoDB measurment script
    Script for performance measurment for login

---
+++ MongoDB measurment code/loggedIn.php
@@ -0,0 +1,61 @@
+<?php
+    include("connection.php");
+    include("performance.php");
+    
+    session_start();
+    if(isset($_SESSION['username'])) {
+        $username = $_SESSION['username'];
+    } else {
+        header("Location: FinalProject.php");
+        exit();
+    }
+
+    echo '<script>savetime();</script>';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SkillTrainy</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="FinalProject.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0">
+</head>
+<body>
+    <div class="username">Welcome <?php echo $username; ?></div>
+    <header>
+        <nav>
+            <ul class="nav-bar">
+                <li class="smallLogo"><a href="loggedIn.php"><span id="miniLogo" class="material-symbols-outlined">exercise</span></a></li>
+                <input type="checkbox" id="check">
+                <span class="menu">
+                    <li><a href="loggedIn.php">Home</a></li>
+                    <li><a href="program.php">Program</a></li>
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="FinalProject.php">Log out</a></li>
+                    <label for="check" class="close-menu"><i class="fas fa-times"></i></label>
+                </span>
+                <label for="check" class="open-menu"><i class="fas fa-bars"></i></label>
+            </ul>
+        </nav>
+    </header>
+    <div id="indexPicture">
+        <img src="../pictures/index.jpg" alt="workout">
+        <p id="indexP">PWR</p>
+        <p id="indexPUnder">POWERWORKOUT</p>
+    </div>
+    <div id="indexWeeks">
+        <p>Week x4</p>
+    </div>
+    <div id="indexUndertext">
+        <p>Elevate your fitness journey with programs uniquely designed to maximize your results.</p>
+    </div>
+    <div class="button-container">
+        <button id="buttonGet"><a href="program.php">Get started</a><span></span></button>
+    </div>
+    <div class="Dots">
+        <p>●○○○</p>
+    </div>
+</body>
+</html>
---
+++ MongoDB measurment code/login.php
@@ -0,0 +1,34 @@
+<?php
+    include("connection.php");
+    include("performance.php");
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $databaseName = 'database2';
+        $collectionName = 'login';
+        
+        $username = $_POST["user"];
+        $password = $_POST["pass"];
+        
+        $filter = ['username' => $username, 'password' => $password];
+        $query = new MongoDB\Driver\Query($filter);
+        $rows = $conn->executeQuery("$databaseName.$collectionName", $query);
+        
+        $match = false;
+        foreach ($rows as $row) {
+            $match = true;
+            break;
+        }
+        
+        if ($match) {
+            session_start();
+            $_SESSION['username'] = $username;
+            echo '<script>savetime();</script>';
+            header("Location: loggedIn.php");
+            exit();
+        } else {
+            echo '<script>alert("Invalid login.");</script>';
+            header("Location: FinalProject.php");
+            exit();
+        }
+    }
+?>
