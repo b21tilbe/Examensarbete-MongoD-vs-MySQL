a92384f7cd8be956b1bb06a6b2075e9eb02830be
b21tilbe <102578990+b21tilbe@users.noreply.github.com>
2024-04-20 13:53:57 +0200

    Update performance.php
    Changed so the correct measurement code is being executed.

--- MySQL measurement code/performance.php
+++ MySQL measurement code/performance.php
@@ -1,54 +1,58 @@
 <?php
+    echo '<script>
+        let iterations = 10;
 
-echo '<script>
-    let iterations=10;
-
-    function savetime()
-    { 
-        let old=performance.now();
-        
-        var arr=[];
-        var it=100000+Math.round(Math.random()*100000);
-        for(var i=0;i<it;i++){
-            arr.push(Math.round(Math.random()*5000));
-        }
-        arr.sort();
-        
-        let measurement=performance.now();
+        function autofillForm() {
+            const usernameInput = document.getElementById("user");
+            const passwordInput = document.getElementById("pass");
     
-        cnt=parseInt(localStorage.getItem("Counter"));
+            usernameInput.value = "Tilda";
+            passwordInput.value = "admin";
 
-        if(cnt>iterations){
-            alert("Finished!"+cnt);
-        }else{
-            if(isNaN(cnt)) cnt=0;
+            const loginButton = document.querySelector(".login-btn");
+            loginButton.click();
 
-            var delta=measurement-old;
-            var str=localStorage.getItem("theData");
-            str+=old+","+measurement+","+delta+"\n";
+            const logoutButton = document.querySelector(\'[href="FinalProject.php"]\');
+
+            logoutButton.click();
+        }
+
+        let measurement=performance.now();
+
+        cnt = parseInt(localStorage.getItem("Counter"));
+
+        if(cnt > iterations) {
+            alert("Finished! " + cnt);
+        } else {
+            if(isNaN(cnt)) cnt = 0;
+
+            let old = performance.now();
+
+            var delta = measurement - old;
+            var str = localStorage.getItem("theData");
+            str += old + "," + measurement + "," + delta + "\\n";
         
-            if(cnt==0){
-                str="data:text/csv;charset=utf-8,";
+            if(cnt == 0) {
+                str = "data:text/csv;charset=utf-8,";
             }
         
             cnt++;
-            localStorage.setItem("Counter",cnt);
-            localStorage.setItem("theData",str);
-        
+            localStorage.setItem("Counter", cnt);
+            localStorage.setItem("theData", str);
+
             location.reload(); 
         }
-    }
 
-    function getData()
-    {
-        var str=localStorage.getItem("theData");
+        function getData() {
+            var str = localStorage.getItem("theData");
 
-        var anchor = document.createElement("a");
-        anchor.setAttribute("href", encodeURI(str));
-        anchor.setAttribute("download", "my_data.csv");
-        anchor.innerHTML= "Click Here to download";
-        document.body.appendChild(anchor);
-        anchor.click();
-    }
-</script>';
+            var anchor = document.createElement("a");
+            anchor.setAttribute("href", encodeURI(str));
+            anchor.setAttribute("download", "my_data.csv");
+            anchor.innerHTML = "Click Here to download";
+            document.body.appendChild(anchor);
+            anchor.click();
+        }
+
+    </script>';
 ?>
